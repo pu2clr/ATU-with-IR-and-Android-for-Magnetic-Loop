b7db343b6a857bcbdfcba0a634c52cdaf02596d7
Ricardo Lima Caratti <pu2clr@gmail.com>
2019-04-14 18:45:58 -0300

    Documentation

--- source/CheckYourRemoteControl/CheckYourRemoteControl.ino
+++ source/CheckYourRemoteControl/CheckYourRemoteControl.ino
@@ -9,16 +9,16 @@ decode_results results;
 void setup()
 {
   Serial.begin(9600);
-  irrecv.enableIRIn(); // Start the receiver
+  irrecv.enableIRIn(); // Initiate the IR Receiver
 
   Serial.println("Ready to receive...");
 }
 
 void loop()
 {
-  // Check the IR
+  // Check the IR. Does it have something?  
   if (irrecv.decode(&results))
-    {
+    { // If so.. 
      Serial.println("Take note. It is the code for this action: ");
      Serial.print(results.value);
      irrecv.resume(); // Ready to receive the next value
